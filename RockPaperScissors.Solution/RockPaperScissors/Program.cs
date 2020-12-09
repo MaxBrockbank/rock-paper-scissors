dec3bddb340fae7b30f4d3a71f0871b1442f3cf5
Eric Endsley <eric.endsley4@gmail.com>
2020-12-09 11:32:27 -0800

    add console UI
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- RockPaperScissors.Solution/RockPaperScissors/Program.cs
+++ RockPaperScissors.Solution/RockPaperScissors/Program.cs
@@ -7,7 +7,13 @@ namespace RPC.Models
     {
       static void Main()
       {
+        Console.WriteLine("Player 1 enter rock, paper or scissors");
+        string player1Input = Console.ReadLine();
+        Console.WriteLine("Player 2 enter rock, paper or scissors -- and don't look at what player1 wrote!!");
+        string player2Input = Console.ReadLine();
 
+        string result = Game.RPCWinner(player1Input, player2Input);
+        Console.WriteLine(result);
       }
     }
 }
