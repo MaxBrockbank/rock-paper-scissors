2bae398dba6733a418769ad3638037c2787fd70d
Eric Endsley <eric.endsley4@gmail.com>
2020-12-09 11:16:17 -0800

    write second test, write RPCWinner functionality to pass
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- RockPaperScissors.Solution/RockPaperScissors.Tests/ModelTests/RockPaperScissors.Tests.cs
+++ RockPaperScissors.Solution/RockPaperScissors.Tests/ModelTests/RockPaperScissors.Tests.cs
@@ -8,13 +8,23 @@ namespace RPC.Tests
   public class GameTests
   {
     [TestMethod]
-    public void RPCWinner_InputsAreSame_StringDraw()
+    public void RPCWinner_InputsAreSame_String()
     {
       string player1Input = "Rock";
       string player2Input = "Rock";
 
       string result = Game.RPCWinner(player1Input, player2Input);
       Assert.AreEqual("Draw", result);
+    }
+
+    [TestMethod]
+    public void RPCWinner_RockWins_String()
+    {
+      string player1Input = "Rock";
+      string player2Input = "Scissors";
+
+      string result = Game.RPCWinner(player1Input, player2Input);
+      Assert.AreEqual("rock wins!", result);
     }
   }
 }
